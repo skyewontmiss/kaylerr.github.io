f3dc3ffa1e7ae8d98de0554e41b1f2701d04b743
Kaylerr <71836631+Kaylerr@users.noreply.github.com>
2023-05-14 18:02:49 +0100

    Update wiimote.js

--- wiimote.js
+++ wiimote.js
@@ -1,9 +1,14 @@
-window.addEventListener('deviceorientation', handleOrientation);
+function update() {
+  window.requestAnimationFrame(update);
+  handleOrientation();
+}
 
-function handleOrientation(event) {
+function handleOrientation() {
   var gyroscopeData = "Gyroscope: alpha=" + event.alpha + ", beta=" + event.beta + ", gamma=" + event.gamma;
   var accelerometerData = "Accelerometer: x=" + event.acceleration.x + ", y=" + event.acceleration.y + ", z=" + event.acceleration.z;
   
   document.getElementById("gyroscopeData").innerHTML = gyroscopeData;
   document.getElementById("accelerometerData").innerHTML = accelerometerData;
 }
+
+update();
